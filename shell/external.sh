5813d37961ff9ee7650190173b41ff189e7f8195
Joseph Sikorski <joedski@gmail.com>
2018-01-14 16:23:45 -0500

    Including global editorconfig.

--- shell/external.sh
+++ shell/external.sh
@@ -14,6 +14,29 @@ export PYTHONSTARTUP=$HOME/.pythonrc
 
 export NVM_DIR="$HOME/.nvm"
 # This loads nvm
-[ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh" || echo "$NVM_DIR/nvm.sh not found."
+if [ -s "$NVM_DIR/nvm.sh" ]; then
+  source "$NVM_DIR/nvm.sh"
+elif [ -s "/usr/local/opt/nvm/nvm.sh" ]; then
+  # Check the other locaation.
+  source "/usr/local/opt/nvm/nvm.sh"
+else
+  echo "$NVM_DIR/nvm.sh not found."
+fi
+
 # This loads nvm bash_completion
 [ -s "$NVM_DIR/bash_completion" ] && source "$NVM_DIR/bash_completion"
+
+
+#### Ruby and RVM
+
+# Load RVM into a shell session *as a function*
+[ -s "$HOME/.rvm/scripts/rvm" ] && source "$HOME/.rvm/scripts/rvm"
+if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
+
+# Add RVM to path.
+PATH=$PATH:$HOME/.rvm/bin
+
+
+#### Local bin
+
+PATH=$PATH:~/bin
