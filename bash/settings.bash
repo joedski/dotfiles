96f4bcdea5a92e0832facb6d71eb56f46d2b606d
Joseph Sikorski <joedski@gmail.com>
2019-05-04 23:46:52 -0400

    Python USER_BASE/bin into PATH; also Sublime Prefs

--- bash/settings.bash
+++ bash/settings.bash
@@ -4,3 +4,11 @@ SAVEHIST=$HISTSIZE
 shopt -s histappend # append to history file
 
 export EDITOR=vim
+
+PYTHON_2_USER_BASE=$(python -c 'import site; print(site.USER_BASE)')
+export PATH="${PATH}:${PYTHON_2_USER_BASE}/bin"
+
+if which python3 > /dev/null; then
+  PYTHON_3_USER_BASE=$(python3 -c 'import site; print(site.USER_BASE)')
+  export PATH="${PATH}:${PYTHON_3_USER_BASE}/bin"
+fi
