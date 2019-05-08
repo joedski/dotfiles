863357298638d64e11ff37b0f08d023cc06109f0
Joseph Sikorski <joedski@gmail.com>
2019-05-08 18:22:17 -0400

    ST3 Plugins: Faffing (dev helper plugin)

--- sublime-text-3/plugin_markdown_helpers.py
+++ sublime-text-3/plugin_markdown_helpers.py
@@ -4,9 +4,9 @@ import sublime_plugin
 
 class MarkdownSetextHeaderH1Command(sublime_plugin.TextCommand):
     def run(self, edit):
-        return
-        # for region in self.view.sel():
-        #     for line in self.view.lines(region):
-
-
-
+        lines = flatten([self.view.lines(region) for region in self.view.sel()])
+        lines.reverse()
+        for line in lines:
+            full_line = self.view.full_line(line)
+            end_class = self.view.classify(full_line.end())
+            print('- {}'.format(end_class & sublime.CLASS_LINE_END))
--- sublime-text-3/plugin_plugin_dev_helper.py
+++ sublime-text-3/plugin_plugin_dev_helper.py
@@ -2,6 +2,13 @@ import sublime
 import sublime_plugin
 
 
+def flatten(list_of_lists):
+    flattened_list = []
+    for sub_list in list_of_lists:
+        for item in sub_list:
+            flattened_list.append(item)
+    return flattened_list
+
 class PluginDevHelperShowViewSelCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         print('Active View Selection Regions:')
@@ -9,4 +16,15 @@ class PluginDevHelperShowViewSelCommand(sublime_plugin.TextCommand):
             print('- [{} {}]'.format(
                 region.begin(),
                 region.end()
+            ))
+
+class PluginDevHelperShowSelectedLinesAndLineLengthsCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        print('Selected Lines and their Lengths:')
+        lines = flatten([self.view.lines(region) for region in self.view.sel()])
+        for line in lines:
+            print('- [{} {}] {}'.format(
+                line.begin(),
+                line.end(),
+                line.size()
             ))
